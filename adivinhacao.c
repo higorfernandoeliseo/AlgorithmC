82328554bda5b52d0cd6aa25e4a8acbbeccc130d
Igor Eliseo <higorfernandoeliseo2016@gmail.com>
2026-03-26 17:48:00 -0300

    removi os comentarios

--- adivinhacao.c
+++ adivinhacao.c
@@ -34,28 +34,7 @@ int main() {
     
 
 
-    // for(int i = 1; i <= 3; i++) {
-    //     printf("Digite o %do chute: ", i);
-    //     scanf("%d", &chute);
-
-    //     int acerto = chute == numerosecreto;
-
-    //     printf("Valor do chute: %d\n", chute);
-
-    //     if(acerto){
-    //         printf("Parabens!! voce acertou!!\n");
-    //         break;
-    //     }else{
-    //         int maior = chute > numerosecreto;
-
-    //         if(maior){
-    //             printf("O chute eh maior que o numero secreto!\n");
-    //         }else{
-    //             printf("O chute eh menor que o numero secreto!\n");
-    //         }
-    //     }
-
-    // }
+   
 
     return 0;
 }
